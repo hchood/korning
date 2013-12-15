49cb1a5b97dcd79a308e05207f7338b59d623069
Helen Hood <helen.c.hood@gmail.com>
2013-12-15 18:20:00 -0500

    now removes $ on sale_amount before importing (was being imported as 0 across the board)

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,17 @@ require 'csv'
 
 datafile = Rails.root + 'db/data/sales.csv'
 
+def remove_dollar_sign(value)
+  value.gsub(/[$]/, '')
+end
+
 CSV.foreach(datafile, headers: true) do |row|
   Sale.find_or_create_by(invoice_no: row['invoice_no']) do |sale|
     sale.employee = row['employee']
     sale.customer_and_account_no = row['customer_and_account_no']
     sale.product_name = row['product_name']
     sale.sale_date = row['sale_date']
-    sale.sale_amount = row['sale_amount']
+    sale.sale_amount = remove_dollar_sign(row['sale_amount'])
     sale.units_sold = row['units_sold']
     sale.invoice_no = row['invoice_no']
     sale.invoice_frequency = row['invoice_frequency']
