61dc047cc516507eb3e5d8d122ddfa3de57e5d35
Helen Hood <helen.c.hood@gmail.com>
2013-12-15 18:40:38 -0500

    modified migration to populate Products table as well as create it

--- db/migrate/20131215233821_create_products.rb
+++ db/migrate/20131215233821_create_products.rb
@@ -5,5 +5,14 @@ class CreateProducts < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    Sale.all.each do |s|
+      unless Product.exists?(name: s.product_name)
+        Product.create(name: s.product_name)
+        puts "#{s.product_name} created!"
+      else
+        puts "#{s.product_name} already in table."
+      end
+    end
   end
 end
