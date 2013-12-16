2dae24d3311dbaec6441f67917f849fe41051423
Helen Hood <helen.c.hood@gmail.com>
2013-12-16 14:35:47 -0500

    modified index action in sales controller to return only sales in the last 13 months

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,6 +1,8 @@
 class SalesController < ApplicationController
+
+ # Displays sales within last 13 months
   def index
-    @sales = Sale.all
+    @sales = Sale.all.where('sale_date BETWEEN :start AND :end', start: 13.months.ago, end: Time.now)
   end
 
   def show
