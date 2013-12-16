aa56a596966ba3dd2f3b5ce280e93c69e47134f6
Helen Hood <helen.c.hood@gmail.com>
2013-12-15 19:03:26 -0500

    added & populated website in Customers

---
+++ db/migrate/20131215235528_add_website_to_customers.rb
@@ -0,0 +1,25 @@
+class AddWebsiteToCustomers < ActiveRecord::Migration
+  def up
+    add_column :customers, :website, :string
+    Customer.all.each do |customer|
+      id = customer.id
+      if id == 1
+        customer.update_attribute(:website, 'http://www.apple.com')
+      elsif id == 2
+        customer.update_attribute(:website, 'http://www.motorola.com')
+      elsif id == 3
+        customer.update_attribute(:website, 'http://www.lg.com')
+      elsif id == 4
+        customer.update_attribute(:website, 'http://www.htc.com')
+      elsif id == 5
+        customer.update_attribute(:website, 'http://www.nokia.com')
+      elsif id == 6
+        customer.update_attribute(:website, 'http://www.samsung.com')
+      end
+    end
+  end
+
+  def down
+    remove_column :customers, :website
+  end
+end
